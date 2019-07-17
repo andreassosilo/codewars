be4284babe3dd9234067d68cb736bb97053fa746
andreassosilo <andreas.sosilo@gmail.com>
2019-07-17 19:16:20 +0700

    Add 8 kyu multiply.js

---
+++ multiply.js
@@ -0,0 +1,8 @@
+// Codewars
+// 8 kyu - Multiply
+
+// Multiply two variable
+let multiply = (a, b) => a * b
+console.time('Performance')
+console.log(multiply(2, 3))
+console.timeEnd('Performance')
