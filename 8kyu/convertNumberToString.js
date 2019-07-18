5121a732b98f5c991b7676c343f928f0ba466ca1
andreassosilo <andreas.sosilo@gmail.com>
2019-07-18 18:18:58 +0700

    Add 8kyu convertNumberToString.js

---
+++ 8kyu/convertNumberToString.js
@@ -0,0 +1,17 @@
+// Codewars
+// 8 kyu - Convert a Number to a String!
+
+/*
+Task:
+-----
+We need a function that can transform a number into a string.
+
+What ways of achieving this do you know?
+*/
+
+function numberToString (num) {
+  return String(num)
+}
+
+// TEST CASES
+console.log(numberToString(123)) // '123'
