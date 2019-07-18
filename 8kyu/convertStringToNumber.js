0ec7356f52b1b6a08fada96ef4c932d2ccc14db9
andreassosilo <andreas.sosilo@gmail.com>
2019-07-18 18:13:29 +0700

    Add 8kyu convertStringToNumber.js

---
+++ 8kyu/convertStringToNumber.js
@@ -0,0 +1,17 @@
+// Codewars
+// 8 kyu - Convert a String to a Number!
+
+/*
+Task:
+-----
+We need a function that can transform a string into a number.
+What ways of achieving this do you know?
+
+Note: Don't worry, all inputs will be strings, and every string is
+a perfectly valid representation of an integral number.
+*/
+
+const stringToNumber = str => Number(str)
+
+// TEST CASES
+console.log(stringToNumber('1234'))
