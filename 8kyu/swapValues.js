af41a325b4f0d2c872b712555842be51a47764d0
andreassosilo <andreas.sosilo@gmail.com>
2019-07-18 11:14:01 +0700

    Add 8kyu swapValues.js

---
+++ 8kyu/swapValues.js
@@ -0,0 +1,16 @@
+// Codewars
+// 8 kyu - Swap Values
+
+/*
+Task:
+-----
+I would like to be able to pass an array with two elements to my
+swapValues function to swap the values. However it appears that the
+values aren't changing. Can you figure out what's wrong here?
+*/
+
+const swapValues = arr => arr.reverse()
+
+// TEST CASES
+var arr = [1, 2]
+console.log(swapValues(arr))
