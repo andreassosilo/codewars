f7d4bc43bcd88f7f3bcd6374988ccbd62c409b98
andreassosilo <andreas.sosilo@gmail.com>
2019-07-22 18:48:41 +0700

    Add 8kyu evenOrOdd.js

---
+++ 8kyu/evenOrOdd.js
@@ -0,0 +1,24 @@
+// Codewars - Andreas Sosilo
+// 8 kyu - Even or Odd
+
+/*
+Task:
+-----
+Get ascii value of character
+
+write a function getASCII which inputs a character and returns the
+corresponding ascii value for that character. Example : getASCII('A') => 65
+*/
+
+function even_or_odd (number) {
+  // Check if the input number is even number or odd number using modulus
+  if (number % 2 === 0) {
+    return 'Even'
+  } else {
+    return 'Odd'
+  }
+}
+
+// TEST CASES
+console.log(even_or_odd(2)) // 'Even'
+console.log(even_or_odd(7)) // 'Odd'
