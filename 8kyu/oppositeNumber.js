61bf03938e7de2302afa9721acc1863724f0eb8d
andreassosilo <andreas.sosilo@gmail.com>
2019-07-23 08:53:49 +0700

    Add 8kyu oppositeNumber.js

---
+++ 8kyu/oppositeNumber.js
@@ -0,0 +1,23 @@
+// Codewars
+// 8 kyu - Opposite number
+
+/*
+Task:
+-----
+Very simple, given a number, find its opposite.
+
+Examples:
+
+1: -1
+14: -14
+-34: 34
+*/
+
+function opposite (number) {
+  // The - operator also have unary version, where they operate only on one variable
+  // - returns its negative counterpart
+  return -number
+}
+
+// TEST CASES
+console.log(opposite(55)) // -55
