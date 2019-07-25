fb5acd6e5d588bad5b45a25b9cdacd7d51d9253d
andreassosilo <andreas.sosilo@gmail.com>
2019-07-25 07:32:16 +0700

    Add 7kyu disemvowelTrolls.js

---
+++ 7kyu/disemvowelTrolls.js
@@ -0,0 +1,26 @@
+// Codewars - Andreas Sosilo
+// 7 kyu - Disemvowel Trolls
+
+/*
+Task:
+-----
+Trolls are attacking your comment section!
+
+A common way to deal with this situation is to remove all of the vowels from the
+trolls' comments, neutralizing the threat.
+
+Your task is to write a function that takes a string and return a new string with all
+vowels removed.
+
+For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+
+Note: for this kata y isn't considered a vowel.
+*/
+
+function disemvowel (str) {
+  // Using RegEx to get new array that filtered out vowel, then join to get a new string
+  return str.match(/[^aeiou]/ig).join('')
+}
+
+// TEST CASES
+console.log(disemvowel('This website is for losers LOL!')) // 'Ths wbst s fr lsrs LL!'
