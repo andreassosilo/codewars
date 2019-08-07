7e122dfb15d1bc61a2bde243adaa48759d127589
andreassosilo <andreas.sosilo@gmail.com>
2019-08-07 21:18:28 +0700

    Refactor 6kyu tribonnaciSequence.js

--- JavaScript/6kyu/tribonacciSequence.js
+++ JavaScript/6kyu/tribonacciSequence.js
@@ -30,10 +30,13 @@ not clearly specified ;)
 */
 
 function tribonacci (signature, n) {
+  // If n = 0, return empty array
   if (n === 0) return []
+  // Using for-loop to finish the tribonnaci sequence
   for (let i = 3; i < n; i++) {
     signature.push(signature[i - 3] + signature[i - 2] + signature[i - 1])
   }
+  // Use slice() to return the n-array
   return signature.slice(0, n)
 }
 
