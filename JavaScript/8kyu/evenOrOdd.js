7468a76ccdc4e9516fe308bc41199d3343e6bc3c
andreassosilo <andreas.sosilo@gmail.com>
2019-10-31 19:51:09 +0700

    Modify function name evenOrOdd

--- JavaScript/8kyu/evenOrOdd.js
+++ JavaScript/8kyu/evenOrOdd.js
@@ -8,11 +8,11 @@ Create a function (or write a script in Shell) that takes an integer as an
 argument and returns "Even" for even numbers or "Odd" for odd numbers.
 */
 
-function even_or_odd (number) {
+function evenOrOdd (number) {
   // Check if the input number is even number or odd number using modulus and ternary
   return number % 2 ? 'Odd' : 'Even'
 }
 
 // TEST CASES
-console.log(even_or_odd(2)) // 'Even'
-console.log(even_or_odd(7)) // 'Odd'
+console.log(evenOrOdd(2)) // 'Even'
+console.log(evenOrOdd(7)) // 'Odd'
