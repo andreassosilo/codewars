c47264e23d1706a81bf9e260db45aba3412d691b
andreassosilo <andreas.sosilo@gmail.com>
2020-03-15 09:52:35 +0100

    Modify 8kyu swapValues.js

--- JavaScript/8kyu/.eslintrc.js
+++ JavaScript/8kyu/.eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     node: true
   },
   extends: [
-    'airbnb'
+    'airbnb-base'
   ],
   globals: {
     Atomics: 'readonly',
--- JavaScript/8kyu/sumOfPositive.js
+++ JavaScript/8kyu/sumOfPositive.js
@@ -1,3 +1,4 @@
+"use strict";
 // Codewars - Andreas Sosilo
 // 8 kyu - Sum of positive
 
@@ -11,17 +12,17 @@ Example [1,-4,7,12] => 1 + 7 + 12 = 20
 Note: if there is nothing to sum, the sum is default to 0.
 */
 
-function positiveSum (arr) {
+function positiveSum(arr) {
   // Create a variable to accomodate the sum of positive numbers
-  let sum = 0
+  let sum = 0;
   // Do looping using for-loop to check all of the elements in the input array
   for (let i = 0; i < arr.length; i++) {
     // Check if it's positif number or not
     if (arr[i] > 0 && typeof arr[i] === 'number') {
-      sum += arr[i]
+      sum += arr[i];
     }
   }
-  return sum
+  return sum;
 }
 
 // TEST CASES
