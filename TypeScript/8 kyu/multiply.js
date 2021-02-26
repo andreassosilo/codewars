75c1c6dd19a0b25a3f3b582047f5b1973f4f357b
Andreas Sosilo <andreas.sosilo@gmail.com>
2021-02-26 09:51:55 +0100

    Add TypeScript - 8 kyu - Multiply

--- TypeScript/8 kyu/multiply.js
+++ TypeScript/8 kyu/multiply.js
@@ -1,15 +1,16 @@
+"use strict";
 // Codewars - Andreas Sosilo
 // 8 kyu - Multiply
-
+exports.__esModule = true;
+exports.multiply = void 0;
 /*
 Task:
 -----
 This code doesn't execute propely. Try to figure out why.
 */
-
-export function multiply(a: number, b: number): number {
-  return a * b;
+function multiply(a, b) {
+    return a * b;
 }
-
+exports.multiply = multiply;
 // TEST CASES
 console.log(multiply(1, 2)); // 2
