96317aee387a60a806bffd1433ee6148341100a3
Andreas Sosilo <andreas.sosilo@gmail.com>
2021-03-04 13:27:08 +0100

    Add TypeScript - 8kyu - Multiply two numbers

---
+++ TypeScript/8 kyu/multiplyTwoNumbers.js
@@ -0,0 +1,14 @@
+"use strict";
+// Codewars - Andreas Sosilo
+// 8 kyu - Function 3 - multiplying two numbers
+exports.__esModule = true;
+exports.multiply = void 0;
+/*
+Task:
+-----
+Implement a function which multiplies two numbers.
+*/
+var multiply = function (num1, num2) { return num1 * num2; };
+exports.multiply = multiply;
+// TEST CASES
+console.log(exports.multiply(2, 3)); // 6
---
+++ TypeScript/8 kyu/multiplyTwoNumbers.ts
@@ -0,0 +1,13 @@
+// Codewars - Andreas Sosilo
+// 8 kyu - Function 3 - multiplying two numbers
+
+/*
+Task:
+-----
+Implement a function which multiplies two numbers.
+*/
+
+export const multiply = (num1: number, num2: number) => num1 * num2;
+
+// TEST CASES
+console.log(multiply(2, 3)); // 6
