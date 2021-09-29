d5f3b6bf8d964d793bfb65e12b24ba0e2fea088f
Andreas Sosilo <andreas.sosilo@gmail.com>
2021-09-29 18:30:19 +0200

    Add 8-kyu grasshopperSayHello.js

---
+++ JavaScript/8kyu/grasshopperSayHello.js
@@ -0,0 +1,19 @@
+'use strict';
+// Codewars - Andreas Sosilo
+// 8 kyu - Grasshopper - Debug sayHello
+
+/*
+Task:
+-----
+The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+
+Example output:
+
+Hello, Mr. Spock
+*/
+
+const sayHello = (name) => `Hello, ${name}`;
+
+// TEST CASES
+console.log(sayHello('Captain Kirk')); // 'Hello, Captain Kirk'
+console.log(sayHello('Mr. Spock')); // 'Hello, Mr. Spock'
