58e020fac57a97a3b6d7edcc2b40c6f1c59dc15f
Andreas Sosilo <andreas.sosilo@gmail.com>
2021-12-12 06:09:20 +0100

    Add 8-kyu JavaScript youCantCodeUnderPressure.js

---
+++ JavaScript/8kyu/youCantCodeUnderPressure1.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// Codewars - Andreas Sosilo
+// 8 kyu - You Can't Code Under Pressure #1
+
+/*
+Task:
+Code as fast as you can! You need to double the integer and return it.
+*/
+
+// SOLUTION
+
+const doubleInteger = (i) => 2 * i;
+
+// TEST CASES
+console.log(doubleInteger(2)); // 4
