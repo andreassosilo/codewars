ecbf8fcb54d259d89eec0497be9e4cac858ce703
Andreas Sosilo <andreas.sosilo@gmail.com>
2022-01-03 20:26:26 +0100

    Add 8-kyu JavaScript basicVariableAssignment.js

---
+++ JavaScript/8kyu/basicVariableAssignment.js
@@ -0,0 +1,17 @@
+'use strict';
+// Codewars - Andreas Sosilo
+// 8 kyu - Basic variable assignment
+
+/*
+Task:
+-----
+This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+*/
+
+// SOLUTION
+const a = 'code';
+const b = 'wa.rs';
+let name = a + b;
+
+// TEST CASES
+console.log(name); // "codewa.rs"
