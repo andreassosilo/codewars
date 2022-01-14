2005f03114ed06655cdb009662223355e028cc34
Andreas Sosilo <andreas.sosilo@gmail.com>
2022-01-14 19:48:36 +0100

    Add 8-kyu JavaScript unusualFive.js

---
+++ JavaScript/8kyu/unusualFive.js
@@ -0,0 +1,18 @@
+'use strict';
+// Codewars - Andreas Sosilo
+// 8 kyu - 5 without numbers !!
+
+/*
+Task:
+-----
+Write a function that always returns 5
+
+Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+Good luck :)
+*/
+
+const unusualFive = () => 'fives'.length;
+
+// TEST CASES
+console.log(unusualFive()); // 5
