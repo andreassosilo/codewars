0dc0c6e3efd04b224a85a07c585b87e5fb983215
Andreas Sosilo <andreas.sosilo@gmail.com>
2022-02-04 20:52:53 +0100

    Add 7-kyu TypeScript boiledEggs.ts

---
+++ TypeScript/7kyu/boiledEggs.js
@@ -0,0 +1,30 @@
+"use strict";
+// Codewars - Andreas Sosilo
+// 7 kyu - TypeScript - MakeUpperCase
+exports.__esModule = true;
+exports.cookingTime = void 0;
+/*
+Task:
+-----
+You are the greatest chef on earth. No one boils eggs like you! Your restaurant is always full of guests, who love your boiled eggs. But when there is a greater order of boiled eggs, you need some time, because you have only one pot for your job. How much time do you need?
+
+Your Task
+Implement a function, which takes a non-negative integer, representing the number of eggs to boil. It must return the time in minutes (integer), which it takes to have all the eggs boiled.
+
+Rules
+you can put at most 8 eggs into the pot at once
+it takes 5 minutes to boil an egg
+we assume, that the water is boiling all the time (no time to heat up)
+for simplicity we also don't consider the time it takes to put eggs into the pot or get them out of it
+Example (Input --> Output)
+0 --> 0
+5 --> 5
+10 --> 10
+*/
+function cookingTime(eggs) {
+    return Math.ceil(eggs / 8) * 5;
+}
+exports.cookingTime = cookingTime;
+// TEST CASES
+console.log(cookingTime(5)); // 5
+console.log(cookingTime(10)); // 10
