e653f2f714fb32d46162c486fef24bda1ae0729e
Andreas Sosilo <andreas.sosilo@gmail.com>
2022-02-15 21:02:35 +0100

    Add 7-kyu TypeScript billiardsTriangle.ts

---
+++ TypeScript/7kyu/billiardsTriangle.ts
@@ -0,0 +1,36 @@
+// Codewars - Andreas Sosilo
+// 7 kyu - TypeScript - Billiards triangle
+
+/*
+Task:
+-----
+Remember the triangle of balls in billiards? To build a classic triangle (5 levels) you need 15 balls. With 3 balls you can build a 2-level triangle, etc.
+
+For more examples,
+
+pyramid(1) == 1
+
+pyramid(3) == 2
+
+pyramid(6) == 3
+
+pyramid(10) == 4
+
+pyramid(15) == 5
+Write a function that takes number of balls (≥ 1) and calculates how many levels you can build a triangle.
+*/
+
+export function pyramid(balls: number): number {
+  let level: number = 0;
+  while (balls > level) {
+    level += 1;
+    balls -= level;
+  }
+  return level;
+}
+
+// TEST CASES
+console.log(pyramid(1)); // 1
+console.log(pyramid(100)); // 13
+console.log(pyramid(2211)); // 66
+console.log(pyramid(9999)); // 140
