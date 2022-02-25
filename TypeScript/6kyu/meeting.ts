415214ba813a02afe0fd2abc3a5ceeac5273fdae
Andreas Sosilo <andreas.sosilo@gmail.com>
2022-02-25 19:11:41 +0100

    Add 6-kyu TypeScript meeting.ts

---
+++ TypeScript/6kyu/meeting.ts
@@ -0,0 +1,40 @@
+// Codewars - Andreas Sosilo
+// 6 kyu - TypeScript - Meeting
+
+/*
+Task:
+-----
+John has invited some friends. His list is:
+
+s = "Fred:Corwill;Wilfred:Corwill;Barney:Tornbull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill";
+Could you make a program that
+
+makes this string uppercase
+gives it sorted in alphabetical order by last name.
+When the last names are the same, sort them by first name. Last name and first name of a guest come in the result between parentheses separated by a comma.
+
+So the result of function meeting(s) will be:
+
+"(CORWILL, ALFRED)(CORWILL, FRED)(CORWILL, RAPHAEL)(CORWILL, WILFRED)(TORNBULL, BARNEY)(TORNBULL, BETTY)(TORNBULL, BJON)"
+It can happen that in two distinct families with the same family name two people have the same first name too.
+*/
+
+export function meeting(s: string): string {
+  return s
+    .split(';')
+    .map((el) => '(' + el.split(':').reverse().join(', ').toUpperCase() + ')')
+    .sort()
+    .join('');
+}
+
+// TEST CASES
+console.log(
+  meeting(
+    'Alexis:Wahl;John:Bell;Victoria:Schwarz;Abba:Dorny;Grace:Meta;Ann:Arno;Madison:STAN;Alex:Cornwell;Lewis:Kern;Megan:Stan;Alex:Korn'
+  )
+); // (ARNO, ANN)(BELL, JOHN)(CORNWELL, ALEX)(DORNY, ABBA)(KERN, LEWIS)(KORN, ALEX)(META, GRACE)(SCHWARZ, VICTORIA)(STAN, MADISON)(STAN, MEGAN)(WAHL, ALEXIS)
+console.log(
+  meeting(
+    'John:Gates;Michael:Wahl;Megan:Bell;Paul:Dorries;James:Dorny;Lewis:Steve;Alex:Meta;Elizabeth:Russel;Anna:Korn;Ann:Kern;Amber:Cornwell'
+  )
+); // (BELL, MEGAN)(CORNWELL, AMBER)(DORNY, JAMES)(DORRIES, PAUL)(GATES, JOHN)(KERN, ANN)(KORN, ANNA)(META, ALEX)(RUSSEL, ELIZABETH)(STEVE, LEWIS)(WAHL, MICHAEL)
